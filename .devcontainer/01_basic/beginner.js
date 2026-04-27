72b1a9f9d7a6b1094bd50ebeb29ae6bfa29c090c
Vivek Ram <158698702+vivekram45@users.noreply.github.com>
2026-04-27 11:44:41 +0000

    container config done

---
+++ .devcontainer/01_basic/beginner.js
@@ -0,0 +1,14 @@
+const accountID="112233"
+let accountname="Vivek Ram"
+var accountemailID="vivekram4005@gmail.com"
+accoutcity="kalyan"
+let accountstate;
+
+console.table([accountID,accountname,accountemailID,accoutcity,accountstate])
+
+// accountID="2231515"
+
+/*
+   var I can not use constantly beacause
+   of block scope and funtional scope
+*/
