19940707c70b749a794e1dbdbc88ffa09ce34454
Vivek Ram <158698702+vivekram45@users.noreply.github.com>
2026-05-18 04:19:10 +0000

    container config done

--- .devcontainer/01_basic/beginner.js
+++ .devcontainer/01_basic/beginner.js
@@ -1,6 +1,6 @@
-const accountID="112233"
-let accountname="Vivek Ram"
-var accountemailID="vivekram4005@gmail.com"
+const accountID="112233"   /*("A constant is a value that remains the same throughout the program" ) */
+let accountname="Vivek Ram" /*(let is also used to declare variable but it is block scoped We can update its value but we cannot redeclare it in the same scope)*/
+var accountemailID="vivekram4005@gmail.com" /*("var is used to declare variable but it is function scoped. We can update its value as well as redeclare it") */
 accoutcity="kalyan"
 let accountstate;
 
@@ -9,6 +9,8 @@ console.table([accountID,accountname,accountemailID,accoutcity,accountstate])
 // accountID="2231515"
 
 /*
+   perfer not to use var
+   because the issues block scope and functional scope
    var I can not use constantly beacause
    of block scope and funtional scope
 */
