998385ccd7a6fd6a0601b266f5b3006677497c72
Vivek Ram <158698702+vivekram45@users.noreply.github.com>
2026-05-18 04:44:26 +0000

    container config dev

---
+++ .devcontainer/01_basic/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+//alert( 3 + 3 )// we are using node js not browser 
+
+let name = "vivek" // string data types
+let age = "21"  // number data types
+let loggedin = "true or false" // boolean data type 
+
+//number  => 2 to power 53
+//bigint 
+//string => ""
+
+//boolen => true or false
+//null => standalone value 
+//undefined => abhi value sign nahi hu ha 
+// symbol => unique 
+
+// object
+
+console.log(typeof undefined);
