7ef87b106a9ddad60d91d27349484ede3a8661ca
Vivek Ram <158698702+vivekram45@users.noreply.github.com>
2026-05-18 05:38:46 +0000

    conversion

---
+++ .devcontainer/01_basic/03_conversionoperation.js
@@ -0,0 +1,25 @@
+let score = null
+
+console.log(typeof (score));
+console.log(typeof score);
+
+let vauleInNumber = Number(score)
+console.log(typeof vauleInNumber);
+console.log( vauleInNumber);
+
+//"33" => 33
+//"33abc" => NAN
+// true => 1 or false = 0
+ 
+let isloggedIn = 1
+let booleanisLoggedIn = Boolean(isloggedIn)
+
+console.log(booleanisLoggedIn);
+
+//"" false
+//"vivek" true
+
+let fewnumber = 5
+let stringNumber = String(fewnumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
